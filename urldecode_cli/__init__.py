b28b7401fd9d61eaeb79748c9f18829d59527641
Annie Ehler <annie.ehler.4@gmail.com>
2024-05-14 10:30:37 -0700

    Had to change the nama bc PyPi

--- urldecode_cli/__init__.py
+++ urldecode_cli/__init__.py
@@ -1,4 +1,4 @@
-__name__ = "urldecode"
+__name__ = "urldecode_cli"
 __version__ = "0.1.0"
 __description__ = "CLI tool to quickly decode URL encoded strings"
 __authors__ = ["Annie Ehler <annie.ehler.4@gmail.com>"]
